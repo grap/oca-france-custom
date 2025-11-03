eed504afa6f618b2342e16e88a2a4c5c47e8339f
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-11-03 21:23:42 +0100

    [ADD] oca_france_base

---
+++ oca_france_base/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025-Today OCA France
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "OCA France Customization - Base Module",
+    "summary": "Base customization for the OCA France Instance",
+    "version": "18.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "GRAP, OCA France",
+    "website": "https://www.oca-france.fr",
+    "depends": ["base"],
+    "data": [
+        "views/view_res_partner.xml",
+    ],
+    "demo": [
+        "demo/res_partner.xml",
+    ],
+}
