b48a267e4742ce34b745e1b5c5f741d35d68386b
Sylvain LE GAL <sylvain.legal@grap.coop>
2025-11-25 00:59:09 +0100

    [REF] oca_france_all: Add new modules

--- oca_france_all/__manifest__.py
+++ oca_france_all/__manifest__.py
@@ -14,10 +14,19 @@
         "oca_france_membership",
         "oca_france_website_membership",
         "oca_france_website_partner",
+        # OCA/geospatial
+        "web_leaflet_lib",
+        "web_view_leaflet_map",
+        "web_view_leaflet_map_partner",
         # OCA/mail
         "mail_debrand",
         # OCA/partner-contact
         "partner_disable_gravatar",
+        "partner_firstname",
+        # OCA/server-auth
+        "user_log_view",
+        # OCA/server-backend
+        "base_user_role",
         # OCA/server-brand
         "disable_odoo_online",
         "portal_odoo_debranding",
@@ -31,6 +40,7 @@
         # OCA/web
         "web_dialog_size",
         "web_editor_disable_chatgpt",
+        "web_favicon",
         "web_no_bubble",
         "web_refresher",
         "web_remember_tree_column_width",
@@ -39,8 +49,8 @@
         "web_theme_classic",
         # OCA/website
         "website_company_mastodon_link",
-        "website_partner_form",
         "website_odoo_debranding",
+        "website_partner_form",
         "website_search_header",
         # Odoo
         "contacts",
